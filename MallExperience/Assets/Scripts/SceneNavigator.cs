c0479496d673ee9c73752210a436a112fd28680c
davidleer <ramirdav@oregonstate.edu>
2019-04-26 14:01:39 -0400

    main scene renamed mainmenu

--- MallExperience/Assets/Scripts/SceneNavigator.cs
+++ MallExperience/Assets/Scripts/SceneNavigator.cs
@@ -8,8 +8,6 @@ public class SceneNavigator : MonoBehaviour
 
     public void LoadMainScene()
     {
-        Debug.Log("STEP 1");
         SceneManager.LoadScene("mainmenu");
-        Debug.Log("STEP 2");
     }
 }
