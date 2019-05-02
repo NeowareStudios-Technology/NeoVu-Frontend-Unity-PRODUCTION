5262b726b6ada73823ad5253caee2e33f7ed69e2
JaredGray <jared.gray19@yahoo.com>
2019-05-02 15:05:43 -0400

    Auto focus testing

---
+++ MallExperience/Assets/Lean/Touch/Scripts/TapAutoFocus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapAutoFocus : MonoBehaviour
+{
+    public GameObject Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = GameObject.Find("AR Script");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("Continuous Check");
+                        switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    Debug.Log("Touch Began");   
+                    break;
+
+                case TouchPhase.Ended:
+                    Debug.Log("Touch Ended");
+                    break;
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0) == true)
+        {
+            Debug.Log("Click Released");
+            Camera.GetComponent<CameraSettings>().TriggerAutofocusEvent();
+        }
+    }
+}
