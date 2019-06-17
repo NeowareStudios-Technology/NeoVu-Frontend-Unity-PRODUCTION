63cdb524585a3fb450d7f86f7e135ad90a82a944
JaredGray <jared.gray19@yahoo.com>
2019-06-17 15:05:36 -0400

    Firebase Login

--- MallExperience/Assets/Scripts/Login.cs
+++ MallExperience/Assets/Scripts/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Auth;
+using Firebase;
 
 public class Login : MonoBehaviour
 {
@@ -24,11 +25,18 @@ public class Login : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+        FirebaseAuth.DefaultInstance.ToString();
         Debug.Log(System.DateTime.Today);
         System.DateTime newDate = System.DateTime.Today.AddYears(-13); //Set the date for 13 or older
         Debug.Log(newDate);
         Debug.Log((System.DateTime.Today - newDate).Days);
-       // Debug.Log(gender.value);
+        if (PlayerPrefs.GetString("Status") != "Signed In")
+        {
+            LogIn();
+        }
+        Debug.LogError(PlayerPrefs.GetString("Status"));
+        Debug.LogWarning(PlayerPrefs.GetString("Email"));
     }
 
     // Update is called once per frame
@@ -82,60 +90,67 @@ public class Login : MonoBehaviour
                 //if password passes check for neccessities of email adress
                 if (userEmail.text.Contains("@") && userEmail.text.Contains("."))
                 {
-                     birthCheck = System.Convert.ToDateTime(birthDate.text);
-                    if ((System.DateTime.Today - birthCheck).Days < 4748) //check to makes sure the age is over 13 years old, in days
+                    System.DateTime temp;
+                    if (System.DateTime.TryParse(birthDate.text, out temp))
                     {
-                        SignUpWarning.text = "Age to low to use this application";
+
+
+                        birthCheck = System.DateTime.Parse(birthDate.text);
+                        if ((System.DateTime.Today - birthCheck).Days < 4748) //check to makes sure the age is over 13 years old, in days
+                        {
+                            SignUpWarning.text = "Age to low to use this application";
+                        }
+                        else
+                        {
+                            bool numCheck = true;
+                            if (phoneNum.text == "")
+                            {
+                                SignUpWarning.text = "Input your phone number";
+                            }
+                            else if ((phoneNum.text.Length > 10) || (phoneNum.text.Length < 9)) //check the phone number length to make sure it is legit
+                            {
+                                SignUpWarning.text = "This is an invalid phone number";
+                            }
+                            else
+                            {
+                                for (int i = 0; i < phoneNum.text.Length; i++) //iterate through the phone number to make sure it is only numeric
+                                {
+                                    if (char.IsLetter(phoneNum.text[i]))
+                                    {
+                                        SignUpWarning.text = "The phone number entered is invalid";
+                                        numCheck = false;
+                                    }
+                                }
+                                if (numCheck == true)
+                                {
+                                    switch (gender.value)  //assign a gender from the drop down
+                                    {
+                                        case 0:
+                                            genderVal = "unspecified";
+                                            break;
+                                        case 1:
+                                            genderVal = "female";
+                                            break;
+                                        case 2:
+                                            genderVal = "male";
+                                            break;
+
+                                    }
+                                    if (gender.value == 4)  //Throw an error if a gender is not picked
+                                    {
+                                        SignUpWarning.tag = "Please pick a gender";
+                                    }
+                                    else
+                                    {
+                                        SignUp();
+                                    }
+                                }
+                            }
+                        }
                     }
                     else
                     {
-                        bool numCheck = true;
-                        if (phoneNum.text == "")
-                        {
-                            SignUpWarning.text = "Input your phone number";
-                        }
-                        else if((phoneNum.text.Length > 10) || (phoneNum.text.Length < 9)) //check the phone number length to make sure it is legit
-                        {
-                            SignUpWarning.text = "This is an invalid phone number";
-                        } 
-                        else
-                        {
-                            for (int i = 0; i < phoneNum.text.Length; i++) //iterate through the phone number to make sure it is only numeric
-                            {
-                                if (char.IsLetter(phoneNum.text[i]))
-                                {
-                                    SignUpWarning.text = "The phone number entered is invalid";
-                                    numCheck = false;
-                                }
-                            }
-                            if (numCheck == true)
-                            {
-                                switch (gender.value)  //assign a gender from the drop down
-                                {
-                                    case 0:
-                                        genderVal = "unspecified";
-                                        break;
-                                    case 1:
-                                        genderVal = "female";
-                                        break;
-                                    case 2:
-                                        genderVal = "male";
-                                        break;
-
-                                }
-                                if (gender.value == 4)  //Throw an error if a gender is not picked
-                                {
-                                    SignUpWarning.tag = "Please pick a gender";
-                                }
-                                else
-                                {
-                                    SignUp();
-                                    signUpScreen.SetActive(false);
-                                    loginWarning.gameObject.SetActive(true);
-                                    loginWarning.text = ("Sign Up Completed");
-                                }
-                            }
-                        }
+                        SignUpWarning.text = "Input a correct date format mm/dd/yyy";
                     }
                 }
                 else
@@ -187,10 +202,14 @@ public class Login : MonoBehaviour
                 }
                 if (task.IsFaulted)
                 {
-                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    SignUpWarning.text = "Email is already in use";
+                    SignUpWarning.gameObject.SetActive(true);
+                    Debug.LogError(task.Exception);
                     return;
                 }
-
+                signUpScreen.SetActive(false);
+                loginWarning.gameObject.SetActive(true);
+                loginWarning.text = ("Sign Up Completed");
                 // Firebase user has been created.
                 Firebase.Auth.FirebaseUser newUser = task.Result;
                 Debug.LogFormat("Firebase user created successfully: {0} ({1})",
@@ -199,18 +218,38 @@ public class Login : MonoBehaviour
                 currUser = newUser;
                 PlayerPrefs.SetFloat("Age", Mathf.Round(((System.DateTime.Today - birthCheck).Days) / 365));
                 PlayerPrefs.SetString("Gender", genderVal);
-                
+
             });
+    }
+    public void loginFailure()
+    {
+        Debug.LogWarning("TestRun");
+        this.GetComponent<AWSManager>().DowloadDataSet("mainmenu");
     }
 
     //Firebase Login Function
     public void LogIn()
     {
-        var email = loginEmail.text;
-        var password = loginPassword.text;
-        
+        string email;
+        string password;
+        if (PlayerPrefs.GetString("Status") != "Signed In")
+        {
+            email = PlayerPrefs.GetString("Email");
+            password = PlayerPrefs.GetString("Password");
+        }
+        else
+        {
+             email = loginEmail.text;
+             password = loginPassword.text;
+        }
+        if(PlayerPrefs.GetString("Status") != "Signed In")
+        {
+            PlayerPrefs.SetString("Email", loginEmail.text);
+            PlayerPrefs.SetString("Password", loginPassword.text);
+        }
 
-        
+
+
         FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
             if (task.IsCanceled)
             {
@@ -220,18 +259,17 @@ public class Login : MonoBehaviour
             if (task.IsFaulted)
             {
                 Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                Debug.LogError("Failure Run");
                 loginWarning.text = "Invalid username or password";
+                //loginFailure();
                 return;
             }
 
             Firebase.Auth.FirebaseUser newUser = task.Result;
-            Debug.LogFormat("User signed in successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
             currUser = newUser;
-            PlayerPrefs.SetString("Status", "Signed In");
             if (newUser.IsEmailVerified)
             {
-                
+                PlayerPrefs.SetString("Status", "Signed In");
                 Debug.Log("Email Verified");
                 this.GetComponent<AWSManager>().DowloadDataSet("mainmenu");
             }
