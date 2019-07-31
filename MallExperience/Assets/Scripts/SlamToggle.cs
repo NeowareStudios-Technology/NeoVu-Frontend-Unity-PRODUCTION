564cda403d2ec505c8b3c511fa47a442972c397b
JaredGray <jared.gray19@yahoo.com>
2019-07-31 13:55:45 -0400

    Slam object updates (Shadows, indicator)

---
+++ MallExperience/Assets/Scripts/SlamToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SlamToggle : MonoBehaviour
+{
+    public AnchorInputListenerBehaviour anchor;
+    public GameObject reticle;
+    public GameObject model;
+    public float high;
+    // Start is called before the first frame update
+    void Start()
+    {
+        high = model.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Place()
+    {
+        anchor.enabled = false;
+        reticle = GameObject.FindGameObjectWithTag("Reticle");
+        reticle.SetActive(false);
+    }
+}
